6bb8306ca7ace63f1b7745938c9a4ccb6707f197
Gustavo Verdugo <gustavoverdugo@MacBook-Pro-de-Admin.local>
2021-05-06 10:34:01 -0400

    add crud functions to product's controller and routes

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -28,6 +28,29 @@ const getProduct = (req, res) => {
     })
 }
 
+const updateProduct = (req, res) => {
+    let productId = req.params.productId
+    let update = req.body
+
+    Products.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+        if (err) res.status(500).send({ message: `Error al actualizar el producto ${err}` })
+
+        res.status(200).send({ message: `Producto Actualizado ${productUpdated}` })
+    })
+}
+
+const deleteProduct = (req, res) => {
+    let productId = req.params.productId;
+    Products.findById(productId, (err, product) => {
+        if (err) res.status(500).send({ message: `Error al borrar el producto ${err}` })
+
+        product.remove(err => {
+            if (err) res.status(500).send({ message: `Error al borrar el producto ${err}` })
+            res.status(200).send({ message: `Producto Borrado` })
+        })
+    })
+}
+
 const saveProduct = (req, res) => {
     let producto = new Products()
     producto.name = req.body.name
@@ -47,5 +70,7 @@ const saveProduct = (req, res) => {
 module.exports = {
     getProducts,
     getProduct,
+    updateProduct,
+    deleteProduct,
     saveProduct
 }
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ let api = express.Router();
 let productController = require('../controllers/products.controller')
 
 api.get('/allpro', productController.getProducts);
-api.get('pro:productId', productController.getProduct);
-
+api.get('/pro:productId', productController.getProduct);
+api.put('/uppro:productId', productController.updateProduct);
+api.delete('/del:productId', productController.deleteProduct);
 api.post('/addpro', productController.saveProduct);
 
 module.exports = api;
